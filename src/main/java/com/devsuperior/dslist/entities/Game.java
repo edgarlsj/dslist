8bfe75f3d18e5d5d1da8c72fd233e1eb4914d40c
edgarlsj <edgarlsj@gmail.com>
2023-11-15 18:13:56 -0300

    criado Properties, Classe Game e ORM no banco

---
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -0,0 +1,126 @@
+package com.devsuperior.dslist.entities;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity//Anotação Spring para que essa classe seja uma tabela no banco de dados relacional
+@Table(name = "tb_game")//Anotação para criar nome na tabela no banco.
+public class Game {
+
+    @Id //Anotação que o campo como chave primária
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //indica como o valor do ID é gerado
+    private Long id;
+    private String title;
+    @Column (name = "game_year")/*Anotação que especifica o mapeamento da coluna no banco
+    nesse caso mudamos o nome da coluna para "game_year"*/
+    private Integer year;
+    private String genre;
+    private String platforms;
+    private Double score;
+    private String imgUrl;
+    private String shortDescription;
+    private String longDescription;
+
+    public Game() {
+    }
+
+    public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl, String shortDescription, String longDescription) {
+        this.id = id;
+        this.title = title;
+        this.year = year;
+        this.genre = genre;
+        this.platforms = platforms;
+        this.score = score;
+        this.imgUrl = imgUrl;
+        this.shortDescription = shortDescription;
+        this.longDescription = longDescription;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getPlatforms() {
+        return platforms;
+    }
+
+    public void setPlatforms(String platforms) {
+        this.platforms = platforms;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public void setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
+    }
+
+    public String getLongDescription() {
+        return longDescription;
+    }
+
+    public void setLongDescription(String longDescription) {
+        this.longDescription = longDescription;
+    }
+
+
+    // metodo equals e hashCode para compara um id com outro IMPORTANTE CRIAR
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Game game = (Game) o;
+        return Objects.equals(id, game.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
